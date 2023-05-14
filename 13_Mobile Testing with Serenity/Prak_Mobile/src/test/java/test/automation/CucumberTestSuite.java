cbf5a2ca5ff415dffac0fa9ac6ea80bf402353f6
ThomasJeff8 <thomasjeff67@gmail.com>
2023-05-14 11:02:46 +0700

    Commit

---
+++ 13_Mobile Testing with Serenity/Prak_Mobile/src/test/java/test/automation/CucumberTestSuite.java
@@ -0,0 +1,12 @@
+package test.automation;
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        stepNotifications = true
+)
+public class CucumberTestSuite {
+}
